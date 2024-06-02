0d8b26cf53139c5c29c9ad64b12ed604b014d2b9
d4rk78 <tymek.marcinkiewicz@wp.pl>
2024-06-03 00:25:38 +0200

    Initial Commit

---
+++ android/app/src/main/java/com/recipetm_proj/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.recipetm_proj
+
+import com.facebook.react.ReactActivity
+
+class MainActivity : ReactActivity() {
+  override fun getMainComponentName(): String? {
+    return "RecipeTM_proj"
+  }
+}
